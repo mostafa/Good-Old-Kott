3a1f847640f8437f94de05b41ba6ae00c2e93600
Mostafa Moradian <mostafamoradian0@gmail.com>
2016-12-20 16:48:25 +0330

    added documentation to kott.py.

--- kott/kott.py
+++ kott/kott.py
@@ -1,6 +1,59 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# TODO: KExceptoin, check data type,
-"""docstring."""
+"""
+    Kott - an abstract data store
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Kott is an abstract data store, where you can store anything
+    and retrieve it easily. The whole idea is around the concept
+    of a key-value store, but has been abstracted to a level that
+    can accept any form, model, data. This means literally everything.
+    Kott is singleton, so always a single instance of it is available.
+
+    >>> from kott import Kott
+    >>> id = Kott().set("my test data which is a string!")
+    >>> print(id)  # e.g. 2a65b1a966c7ddc4cb86c9b75e3dbcea
+    >>> Kott().get(id)
+    my test data which is a string!
+
+    As you can see, there is almost no need for storing the keys or
+    saving them somewhere, because you can always find your data.
+    For example, you can store the instances of your classes in Kott
+    and later access them using their keys. The magic happens with
+    the concept and implementation of Kott plugins or KPlugs.
+    Everything beyond the concept of key-value store is written
+    as kplug. There are official (bundled) kplugs, such as KTag,
+    KTimerPool, KString, ... . Every kplug adds its own keywords
+    to the core of Kott. So, if you want to tag your data while
+    inserting it into Kott, you just use the keyword `tag` as follows.
+    KPlugs are also singleton, and they have priorities while running,
+    so, a kplug with a lower value is executed before a kplug with a
+    higher value. To demonstrate the power and flexibility of Kott,
+    have a look at the example below. Official kplugs are called and
+    loaded by their name as string, but custom kplugs (written by users)
+    should be passed as instance.
+
+    >>> Kott().load_kplug("KTag")  # True if loaded, False otherwise
+    True
+    >>> Kott().set(123, tag="my_numbers")
+    >>> Kott().set(456, tag="my_numbers")
+    >>> Kott().set(789, tag="my_numbers")
+    >>> ids = Kott().find(tag="my_numbers")
+    >>> for id in ids:
+    ...     Kott().get(id)
+    123
+    456
+    789
+
+    The find method looks for keywords in each kplug, and starts searching
+    one by one to find the matching value, then it returns their IDs.
+
+    A sample kplug, KSample, is provided as a example for kplug developers.
+
+    :copyright: (c) 2016 by Hossein Ghannad, Sina Hatefmatbue, Mostafa Moradian
+    :license: TBD, see LICENSE for more details.
+
+"""
 
 from kcore.ksingleton import kSingleton
 from kcore import krand
